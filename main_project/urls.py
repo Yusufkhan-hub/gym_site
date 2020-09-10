1fdfb28f40477df55f8d302492dab6c237bcb15d
Yusufkhan-hub <57767821+Yusufkhan-hub@users.noreply.github.com>
2020-09-10 15:12:13 +0530

    settings done

--- main_project/urls.py
+++ main_project/urls.py
@@ -18,9 +18,17 @@ from django.urls import path,include
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('gym_site.urls')),
     
     path('',TemplateView.as_view(template_name='index.html'),name='index'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
